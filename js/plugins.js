f084dc5f769e0b71c9b75fd742eca3cb4186ffea
Amr <amr.ouf96@gmail.com>
2019-07-15 11:01:01 +0200

    Updated main JS file

--- js/plugins.js
+++ js/plugins.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-
+	
 	$(".wallet .drop-down h6").click(function(){
 		$(this).parent().siblings().find("p").addClass("hidden");
 		$(this).next().toggleClass("hidden");
-
 	});
 
 });
